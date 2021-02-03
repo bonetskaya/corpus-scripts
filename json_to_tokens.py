50eb507f1073fda08511b68728228056c463dbe0
olybo <olkabony@ya.ru>
2021-02-03 22:44:09 +0300

    fix args processing for tokenizer

--- json_to_tokens.py
+++ json_to_tokens.py
@@ -7,11 +7,11 @@ from nltk.tokenize import word_tokenize
 
 parser = argparse.ArgumentParser(description="Get tokenized sentences from json. Output example:\n русский ||| 中文")
 parser.add_argument("filenames", nargs="+", type=str, help="json filenames")
-parser.add_argument("-t", nargs=1, type=str, help="tokenization type (nltk)", default=["nltk"])
+parser.add_argument("-t", "--tokenizer", nargs=1, type=str, help="tokenization type (nltk)", default=["nltk"])
 
 parser_args = parser.parse_args()
 filenames = parser_args.filenames
-tokenization = parser_args.t[0]
+tokenization = parser_args.tokenizer[0]
 
 
 all_sentences = {}
