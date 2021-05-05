5a07bb6959eacd994bb7d618a41ff58c129f2611
olybo <olkabony@ya.ru>
2021-05-05 21:20:20 +0300

    fix json_to_tokens tokenization to real corpus tokenization

--- json_to_tokens.py
+++ json_to_tokens.py
@@ -2,16 +2,22 @@
 
 import argparse
 import json
-from nltk.tokenize import word_tokenize
+
+
+class Sentence:
+    text: str
+    lang: int
+
+    def __init__(self, text, lang):
+        self.text = text
+        self.lang = lang
 
 
 parser = argparse.ArgumentParser(description="Get tokenized sentences from json. Output example:\n русский ||| 中文")
 parser.add_argument("filenames", nargs="+", type=str, help="json filenames")
-parser.add_argument("-t", "--tokenizer", nargs=1, type=str, help="tokenization type (nltk)", default=["nltk"])
 
 parser_args = parser.parse_args()
 filenames = parser_args.filenames
-tokenizer = parser_args.tokenizer[0]
 
 
 all_sentences = {}
@@ -20,19 +26,17 @@ for filename in filenames:
     with open(filename) as json_file:
         data_json = json.load(json_file)
         for sentence_json in data_json["sentences"]:
-            sentence = sentence_json["text"]
+            sentence = " ".join([word["wf"] for word in sentence_json["words"]]).replace("\n", "")
             id = sentence_json["para_alignment"][0]["para_id"]
             lang = sentence_json["lang"]
-            all_sentences[id] = all_sentences.get(id, []) + [(lang, sentence)]
+            all_sentences[id] = all_sentences.get(id, []) + [Sentence(sentence, lang)]
 
     result = []
     for sentence_pair in all_sentences.values():
         if len(sentence_pair) != 2:
             continue
-        if len(sentence_pair[0][1]) == 0 or len(sentence_pair[1][1]) == 0:
+        if len(sentence_pair[0].text) == 0 or len(sentence_pair[1].text) == 0:
             continue
-        if sentence_pair[0][0] != 0:
+        if sentence_pair[0].lang != 0:
             sentence_pair = (sentence_pair[1], sentence_pair[0])
-        rus = word_tokenize(sentence_pair[0][1])
-        zh = word_tokenize(sentence_pair[1][1])
-        print(" ".join(rus) + " ||| " + " ".join(zh))
+        print(sentence_pair[0].text + " ||| " + sentence_pair[1].text)
